b39e443f4324a5e78054141d3c0cfbc504d6b9b0
ali6254 <phonne6254@gmail.com>
2023-09-19 14:29:58 -0400

    Scanner Example

---
+++ project2/src/projectweek2/ScannerExample.java
@@ -0,0 +1,19 @@
+package projectweek2;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter your name");
+		String name = input.nextLine();
+		
+		
+		System.out.println("my name is "+name);
+		// TODO Auto-generated method stub
+
+	}
+
+}
