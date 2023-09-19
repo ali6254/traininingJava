fa985eea466f66d37dca70e2b3180b3143e5d0cd
ali6254 <phonne6254@gmail.com>
2023-09-19 14:09:21 -0400

    update includ exempls from the slides escapCharacter

---
+++ project2/src/projectweek2/EscapCharacters.java
@@ -0,0 +1,18 @@
+package projectweek2;
+
+public class EscapCharacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Boolean wen = new Boolean (false) ;
+		System.out.println("1\tone\tcol3 AND ONE SLASH\\ ");
+		System.out.println("this is a tab charcter ->\t<- ");
+		System.out.println("1\tOne\t\tCol3 ");
+		System.out.println("10\tOne\tcol3 ");
+		System.out.println("1\tone\tcol3\\ ");
+		System.out.println("line1\nline2\nline3 ");
+		System.out.println("1\bcheck\fthisf ");
+
+	}
+
+}
